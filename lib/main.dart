51bcc02b67b1fa68aa7051a8333b9333ec5964cc
Chloe R. Rowley <chloeruthrowley2002@gmail.com>
2023-08-18 12:57:10 +0530

    created custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,28 +4,35 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 248, 255, 53),
-                Color.fromARGB(255, 255, 253, 145),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(
-                color: Colors.black87,
-                fontSize: 30,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.amber,
+            Colors.yellow,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            color: Colors.black87,
+            fontSize: 30,
+          ),
+        ),
+      ),
+    );
+  }
 }
